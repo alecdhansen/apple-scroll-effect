187541b00a9af6e0374897f21294b6d30f231ec3
Alec Hansen <alecdhansen@gmail.com>
2023-03-08 16:02:29 -0500

    refactor img initial rendere

--- src/components/ScrollEffect/index.tsx
+++ src/components/ScrollEffect/index.tsx
@@ -20,15 +20,27 @@ const ScrollEffect = () => {
       Math.ceil(((scrollpx / windowHeightPx) * 100) / 0.69) + 1;
     setScrolled(scrollLength);
   };
-  useEffect(() => {
-    const images: string[] = [];
-    console.log({ images });
-    for (let i = 1; i <= 147; i++) {
-      images.push(
-        (new Image().src = `AirPodImages/${i.toString().padStart(4, "0")}.jpg`)
-      );
+  // const images: string[] = [];
+  // useEffect(() => {
+  //   console.log({ images });
+  //   for (let i = 1; i <= 147; i++) {
+  //     images.push(
+  //       (new Image().src = `AirPodImages/${i.toString().padStart(4, "0")}.jpg`)
+  //     );
+  //   }
+  // }, []);
+
+  const preloadImages = () => {
+    for (let i = 1; i < 147; i++) {
+      const img = new Image();
+      img.src = `AirPodImages/${i.toString().padStart(4, "0")}.jpg`;
     }
+  };
+
+  useEffect(() => {
+    preloadImages();
   }, []);
+
   useEffect(() => {
     window.addEventListener("scroll", scrollProgress);
     return () => window.removeEventListener("scroll", scrollProgress);
