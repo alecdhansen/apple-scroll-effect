feb6ac5cca3e31ed627723924b665ac76134a06d
Alec Hansen <alecdhansen@gmail.com>
2023-04-01 21:46:35 -0400

    rip out old functionality/implement new method

--- src/components/ScrollEffect/index.tsx
+++ src/components/ScrollEffect/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { MutableRefObject, useEffect, useRef, useState } from "react";
 import {
   CanvasContainer,
   Container,
@@ -10,51 +10,61 @@ import {
 } from "./style";
 
 const ScrollEffect = () => {
-  const [scrolled, setScrolled] = useState<number>(1);
-  const scrollProgress = () => {
-    const scrollpx = document.documentElement.scrollTop;
-    const windowHeightPx =
-      document.documentElement.scrollHeight -
-      document.documentElement.clientHeight;
-    const scrollLength =
-      Math.ceil(((scrollpx / windowHeightPx) * 100) / 0.69) + 1;
-    setScrolled(scrollLength);
-  };
-  // const images: string[] = [];
-  // useEffect(() => {
-  //   console.log({ images });
-  //   for (let i = 1; i <= 147; i++) {
-  //     images.push(
-  //       (new Image().src = `AirPodImages/${i.toString().padStart(4, "0")}.jpg`)
-  //     );
-  //   }
-  // }, []);
-
-  const preloadImages = () => {
-    for (let i = 1; i < 147; i++) {
-      const img = new Image();
-      img.src = `AirPodImages/${i.toString().padStart(4, "0")}.jpg`;
-    }
+  const [scrolled, setScrolled] = useState<number>(0);
+  const [images, setImages] = useState<HTMLImageElement[]>([]);
+  const [airpodsHeight, setAirpodsHeight] = useState<number>(0);
+  const frameCount = 146;
+  const [scrollYPosition, setScrollYPosition] = useState<number>(0);
+  const airpodsContainer = useRef() as MutableRefObject<HTMLDivElement>;
+  const handleScroll = () => {
+    setScrollYPosition(window.pageYOffset);
   };
 
   useEffect(() => {
+    setAirpodsHeight(airpodsContainer.current?.offsetHeight);
     preloadImages();
   }, []);
 
   useEffect(() => {
-    window.addEventListener("scroll", scrollProgress);
-    return () => window.removeEventListener("scroll", scrollProgress);
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
+
+  useEffect(() => {
+    if (scrollYPosition > 0 && scrollYPosition <= airpodsHeight) {
+      setScrolled(Math.floor(scrollYPosition / (airpodsHeight / frameCount)));
+    }
+    if (
+      Math.floor(scrollYPosition / (airpodsHeight / frameCount)) > frameCount
+    ) {
+      setScrolled(frameCount);
+    }
+    if (scrollYPosition === 0) {
+      setScrolled(0);
+    }
+  }, [scrollYPosition]);
+
+  const preloadImages = () => {
+    for (let i = 1; i <= frameCount; i++) {
+      const img: HTMLImageElement = new Image();
+      img.src = `/AirPodImages/${i.toString().padStart(4, "0")}.jpg`;
+      setImages((prevImages: HTMLImageElement[]) => [...prevImages, img]);
+    }
+  };
+
+  const src =
+    scrolled >= frameCount
+      ? images[frameCount].src
+      : images[0] && images[scrolled].src;
+
   return (
     <>
-      <Container>
+      <Container ref={airpodsContainer}>
         <CanvasContainer>
           <ImageContainer>
-            <TestImage
-              src={`AirPodImages/${scrolled.toString().padStart(4, "0")}.jpg`}
-              alt=""
-              loading="eager"
-            />
+            <TestImage src={src} alt="" loading="eager" />
           </ImageContainer>
           <Text>Apple AirPods</Text>
         </CanvasContainer>
